9e8fc812ad83671972cda9396fb3495def7ec96a
Pablo González <pagoru@gmail.com>
2017-05-29 00:31:01 +0100

    First and last commit

---
+++ vb-projectP03/Form1.Designer.vb
@@ -0,0 +1,426 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.password = New System.Windows.Forms.TextBox()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.username = New System.Windows.Forms.TextBox()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.country = New System.Windows.Forms.TextBox()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.postalcode = New System.Windows.Forms.TextBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.state = New System.Windows.Forms.TextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.address2 = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.address1 = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.lastname = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.firstname = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.memberid = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.genusername = New System.Windows.Forms.Button()
+        Me.buscar = New System.Windows.Forms.Button()
+        Me.buscar_box = New System.Windows.Forms.TextBox()
+        Me.eliminar = New System.Windows.Forms.Button()
+        Me.editar = New System.Windows.Forms.Button()
+        Me.afegir = New System.Windows.Forms.Button()
+        Me.cancelar = New System.Windows.Forms.Button()
+        Me.acceptar = New System.Windows.Forms.Button()
+        Me.anterior = New System.Windows.Forms.Button()
+        Me.primer = New System.Windows.Forms.Button()
+        Me.ultim = New System.Windows.Forms.Button()
+        Me.seguent = New System.Windows.Forms.Button()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.password)
+        Me.GroupBox1.Controls.Add(Me.Label10)
+        Me.GroupBox1.Controls.Add(Me.username)
+        Me.GroupBox1.Controls.Add(Me.Label9)
+        Me.GroupBox1.Controls.Add(Me.country)
+        Me.GroupBox1.Controls.Add(Me.Label8)
+        Me.GroupBox1.Controls.Add(Me.postalcode)
+        Me.GroupBox1.Controls.Add(Me.Label7)
+        Me.GroupBox1.Controls.Add(Me.state)
+        Me.GroupBox1.Controls.Add(Me.Label6)
+        Me.GroupBox1.Controls.Add(Me.address2)
+        Me.GroupBox1.Controls.Add(Me.Label5)
+        Me.GroupBox1.Controls.Add(Me.address1)
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Controls.Add(Me.lastname)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.firstname)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.memberid)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Location = New System.Drawing.Point(13, 13)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(227, 297)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Membre"
+        '
+        'password
+        '
+        Me.password.Location = New System.Drawing.Point(112, 254)
+        Me.password.Name = "password"
+        Me.password.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
+        Me.password.Size = New System.Drawing.Size(100, 20)
+        Me.password.TabIndex = 19
+        '
+        'Label10
+        '
+        Me.Label10.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label10.Location = New System.Drawing.Point(7, 254)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(99, 20)
+        Me.Label10.TabIndex = 18
+        Me.Label10.Text = "Password"
+        Me.Label10.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'username
+        '
+        Me.username.Location = New System.Drawing.Point(112, 228)
+        Me.username.Name = "username"
+        Me.username.Size = New System.Drawing.Size(100, 20)
+        Me.username.TabIndex = 17
+        '
+        'Label9
+        '
+        Me.Label9.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label9.Location = New System.Drawing.Point(7, 228)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(99, 20)
+        Me.Label9.TabIndex = 16
+        Me.Label9.Text = "Username"
+        Me.Label9.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'country
+        '
+        Me.country.Location = New System.Drawing.Point(112, 202)
+        Me.country.Name = "country"
+        Me.country.Size = New System.Drawing.Size(100, 20)
+        Me.country.TabIndex = 15
+        '
+        'Label8
+        '
+        Me.Label8.Location = New System.Drawing.Point(7, 202)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(99, 20)
+        Me.Label8.TabIndex = 14
+        Me.Label8.Text = "Country"
+        Me.Label8.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'postalcode
+        '
+        Me.postalcode.Location = New System.Drawing.Point(112, 176)
+        Me.postalcode.Name = "postalcode"
+        Me.postalcode.Size = New System.Drawing.Size(100, 20)
+        Me.postalcode.TabIndex = 13
+        '
+        'Label7
+        '
+        Me.Label7.Location = New System.Drawing.Point(7, 176)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(99, 20)
+        Me.Label7.TabIndex = 12
+        Me.Label7.Text = "PostalCode"
+        Me.Label7.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'state
+        '
+        Me.state.Location = New System.Drawing.Point(112, 150)
+        Me.state.Name = "state"
+        Me.state.Size = New System.Drawing.Size(100, 20)
+        Me.state.TabIndex = 11
+        '
+        'Label6
+        '
+        Me.Label6.Location = New System.Drawing.Point(7, 150)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(99, 20)
+        Me.Label6.TabIndex = 10
+        Me.Label6.Text = "State"
+        Me.Label6.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'address2
+        '
+        Me.address2.Location = New System.Drawing.Point(112, 124)
+        Me.address2.Name = "address2"
+        Me.address2.Size = New System.Drawing.Size(100, 20)
+        Me.address2.TabIndex = 9
+        '
+        'Label5
+        '
+        Me.Label5.Location = New System.Drawing.Point(7, 124)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(99, 20)
+        Me.Label5.TabIndex = 8
+        Me.Label5.Text = "Address2"
+        Me.Label5.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'address1
+        '
+        Me.address1.Location = New System.Drawing.Point(112, 98)
+        Me.address1.Name = "address1"
+        Me.address1.Size = New System.Drawing.Size(100, 20)
+        Me.address1.TabIndex = 7
+        '
+        'Label4
+        '
+        Me.Label4.Location = New System.Drawing.Point(7, 98)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(99, 20)
+        Me.Label4.TabIndex = 6
+        Me.Label4.Text = "Address1"
+        Me.Label4.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'lastname
+        '
+        Me.lastname.Location = New System.Drawing.Point(112, 72)
+        Me.lastname.Name = "lastname"
+        Me.lastname.Size = New System.Drawing.Size(100, 20)
+        Me.lastname.TabIndex = 5
+        '
+        'Label3
+        '
+        Me.Label3.Location = New System.Drawing.Point(7, 72)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(99, 20)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "LastName"
+        Me.Label3.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'firstname
+        '
+        Me.firstname.Location = New System.Drawing.Point(112, 46)
+        Me.firstname.Name = "firstname"
+        Me.firstname.Size = New System.Drawing.Size(100, 20)
+        Me.firstname.TabIndex = 3
+        '
+        'Label2
+        '
+        Me.Label2.Location = New System.Drawing.Point(7, 46)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(99, 20)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "FirstName"
+        Me.Label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'memberid
+        '
+        Me.memberid.Location = New System.Drawing.Point(112, 20)
+        Me.memberid.Name = "memberid"
+        Me.memberid.ReadOnly = True
+        Me.memberid.Size = New System.Drawing.Size(100, 20)
+        Me.memberid.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.Location = New System.Drawing.Point(7, 20)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(99, 20)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "MemberID"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'genusername
+        '
+        Me.genusername.Location = New System.Drawing.Point(246, 241)
+        Me.genusername.Name = "genusername"
+        Me.genusername.Size = New System.Drawing.Size(100, 20)
+        Me.genusername.TabIndex = 1
+        Me.genusername.Text = "Gen. username"
+        Me.genusername.UseVisualStyleBackColor = True
+        '
+        'buscar
+        '
+        Me.buscar.Location = New System.Drawing.Point(352, 33)
+        Me.buscar.Name = "buscar"
+        Me.buscar.Size = New System.Drawing.Size(100, 20)
+        Me.buscar.TabIndex = 2
+        Me.buscar.Text = "Buscar per id"
+        Me.buscar.UseVisualStyleBackColor = True
+        '
+        'buscar_box
+        '
+        Me.buscar_box.Location = New System.Drawing.Point(246, 33)
+        Me.buscar_box.Name = "buscar_box"
+        Me.buscar_box.Size = New System.Drawing.Size(100, 20)
+        Me.buscar_box.TabIndex = 20
+        '
+        'eliminar
+        '
+        Me.eliminar.Location = New System.Drawing.Point(246, 136)
+        Me.eliminar.Name = "eliminar"
+        Me.eliminar.Size = New System.Drawing.Size(100, 20)
+        Me.eliminar.TabIndex = 21
+        Me.eliminar.Text = "Eliminar"
+        Me.eliminar.UseVisualStyleBackColor = True
+        '
+        'editar
+        '
+        Me.editar.Location = New System.Drawing.Point(246, 163)
+        Me.editar.Name = "editar"
+        Me.editar.Size = New System.Drawing.Size(100, 20)
+        Me.editar.TabIndex = 22
+        Me.editar.Text = "Editar"
+        Me.editar.UseVisualStyleBackColor = True
+        '
+        'afegir
+        '
+        Me.afegir.Location = New System.Drawing.Point(246, 189)
+        Me.afegir.Name = "afegir"
+        Me.afegir.Size = New System.Drawing.Size(100, 20)
+        Me.afegir.TabIndex = 23
+        Me.afegir.Text = "Afegir nou"
+        Me.afegir.UseVisualStyleBackColor = True
+        '
+        'cancelar
+        '
+        Me.cancelar.Location = New System.Drawing.Point(246, 267)
+        Me.cancelar.Name = "cancelar"
+        Me.cancelar.Size = New System.Drawing.Size(100, 20)
+        Me.cancelar.TabIndex = 24
+        Me.cancelar.Text = "Cancel·lar"
+        Me.cancelar.UseVisualStyleBackColor = True
+        Me.cancelar.Visible = False
+        '
+        'acceptar
+        '
+        Me.acceptar.Location = New System.Drawing.Point(352, 267)
+        Me.acceptar.Name = "acceptar"
+        Me.acceptar.Size = New System.Drawing.Size(100, 20)
+        Me.acceptar.TabIndex = 25
+        Me.acceptar.Text = "Acceptar"
+        Me.acceptar.UseVisualStyleBackColor = True
+        Me.acceptar.Visible = False
+        '
+        'anterior
+        '
+        Me.anterior.Enabled = False
+        Me.anterior.Location = New System.Drawing.Point(125, 316)
+        Me.anterior.Name = "anterior"
+        Me.anterior.Size = New System.Drawing.Size(100, 20)
+        Me.anterior.TabIndex = 26
+        Me.anterior.Text = "<"
+        Me.anterior.UseVisualStyleBackColor = True
+        '
+        'primer
+        '
+        Me.primer.Enabled = False
+        Me.primer.Location = New System.Drawing.Point(13, 316)
+        Me.primer.Name = "primer"
+        Me.primer.Size = New System.Drawing.Size(100, 20)
+        Me.primer.TabIndex = 27
+        Me.primer.Text = "<<"
+        Me.primer.UseVisualStyleBackColor = True
+        '
+        'ultim
+        '
+        Me.ultim.Location = New System.Drawing.Point(352, 316)
+        Me.ultim.Name = "ultim"
+        Me.ultim.Size = New System.Drawing.Size(100, 20)
+        Me.ultim.TabIndex = 28
+        Me.ultim.Text = ">>"
+        Me.ultim.UseVisualStyleBackColor = True
+        '
+        'seguent
+        '
+        Me.seguent.Location = New System.Drawing.Point(246, 316)
+        Me.seguent.Name = "seguent"
+        Me.seguent.Size = New System.Drawing.Size(100, 20)
+        Me.seguent.TabIndex = 29
+        Me.seguent.Text = ">"
+        Me.seguent.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(464, 349)
+        Me.Controls.Add(Me.seguent)
+        Me.Controls.Add(Me.ultim)
+        Me.Controls.Add(Me.primer)
+        Me.Controls.Add(Me.anterior)
+        Me.Controls.Add(Me.acceptar)
+        Me.Controls.Add(Me.cancelar)
+        Me.Controls.Add(Me.afegir)
+        Me.Controls.Add(Me.editar)
+        Me.Controls.Add(Me.eliminar)
+        Me.Controls.Add(Me.buscar_box)
+        Me.Controls.Add(Me.buscar)
+        Me.Controls.Add(Me.genusername)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Name = "Form1"
+        Me.ShowIcon = False
+        Me.Text = "Pablo González Rubio"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents password As TextBox
+    Friend WithEvents Label10 As Label
+    Friend WithEvents username As TextBox
+    Friend WithEvents Label9 As Label
+    Friend WithEvents country As TextBox
+    Friend WithEvents Label8 As Label
+    Friend WithEvents postalcode As TextBox
+    Friend WithEvents Label7 As Label
+    Friend WithEvents state As TextBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents address2 As TextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents address1 As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents lastname As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents firstname As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents memberid As TextBox
+    Protected Friend WithEvents Label1 As Label
+    Friend WithEvents genusername As Button
+    Friend WithEvents buscar As Button
+    Friend WithEvents buscar_box As TextBox
+    Friend WithEvents eliminar As Button
+    Friend WithEvents editar As Button
+    Friend WithEvents afegir As Button
+    Friend WithEvents cancelar As Button
+    Friend WithEvents acceptar As Button
+    Friend WithEvents anterior As Button
+    Friend WithEvents primer As Button
+    Friend WithEvents ultim As Button
+    Friend WithEvents seguent As Button
+End Class
---
+++ vb-projectP03/Form1.vb
@@ -0,0 +1,288 @@
+﻿Imports System.Data.SqlClient
+
+Public Class Form1
+
+    Private conn As SqlConnection
+    Private dts As DataSet
+    Private adapter As SqlDataAdapter
+    Private bmb As BindingManagerBase
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        EmptyMember(True)
+
+        conn = New SqlConnection()
+        conn.ConnectionString = "Data Source=.\SQLEXPRESS;Initial Catalog=COMPASSTRAVEL;Trusted_Connection=true;"
+        conn.Open()
+
+        adapter = New SqlDataAdapter("SELECT * FROM members ORDER BY memberid ASC", conn)
+        Dim builder As SqlCommandBuilder = New SqlCommandBuilder(adapter)
+
+        dts = New DataSet()
+        adapter.Fill(dts, "Members")
+
+        FillDataBindings()
+
+    End Sub
+
+    Private Sub FillDataBindings()
+        Dim textboxes As TextBox() = {memberid, firstname, lastname, address1, address2, state, postalcode, country, username, password}
+        Dim rows As String() = {"MEMBERID", "FIRSTNAME", "LASTNAME", "ADDRESS1", "ADDRESS2", "STATE", "POSTALCODE", "COUNTRY", "USERNAME", "PASSWORD"}
+
+        For idx As Integer = 0 To textboxes.Length - 1 Step 1
+            textboxes(idx).DataBindings.Add(New Binding("Text", dts, $"Members.{rows(idx)}"))
+        Next
+        bmb = BindingContext(dts, "Members")
+
+        If bmb.Count > 0 Then
+            EmptyMember(False)
+        End If
+        updateControllers()
+    End Sub
+
+    Private Sub Leftcontrols(opt As Boolean)
+        primer.Enabled = opt
+        anterior.Enabled = opt
+    End Sub
+
+    Private Sub RightControls(opt As Boolean)
+        ultim.Enabled = opt
+        seguent.Enabled = opt
+    End Sub
+
+    Private Sub ToggleNewMembre()
+        cancelar.Visible = Not cancelar.Visible
+        acceptar.Visible = Not acceptar.Visible
+
+        eliminar.Visible = Not eliminar.Visible
+        editar.Visible = Not editar.Visible
+        afegir.Visible = Not afegir.Visible
+        primer.Visible = Not primer.Visible
+        anterior.Visible = Not anterior.Visible
+        seguent.Visible = Not seguent.Visible
+        ultim.Visible = Not ultim.Visible
+        buscar.Visible = Not buscar.Visible
+        buscar_box.Visible = Not buscar_box.Visible
+    End Sub
+
+    Private Sub afegir_Click(sender As Object, e As EventArgs) Handles afegir.Click
+        ToggleNewMembre()
+        GenerateNewID()
+
+        EmptyMember(False)
+    End Sub
+
+    Private Sub cancelar_Click(sender As Object, e As EventArgs) Handles cancelar.Click
+        ToggleNewMembre()
+
+        bmb.CancelCurrentEdit()
+
+        If bmb.Count = 0 Then
+            EmptyMember(True)
+        End If
+    End Sub
+
+    Private Sub acceptar_Click(sender As Object, e As EventArgs) Handles acceptar.Click
+
+        If Not VerifyMember() Then
+            Return
+        End If
+
+        ToggleNewMembre()
+
+        bmb.EndCurrentEdit()
+        SaveChanges()
+        updateControllers()
+
+    End Sub
+
+    Private Function VerifyMember()
+
+        If password.Text.Length < 4 Then
+            MsgBox("La contrasenya ha de tenir mes de 4 caracters")
+            Return False
+        End If
+
+        If password.Text.Length > 4 And password.Text.Length < 8 Then
+            MsgBox("La contrasenya es valida pero hauria de ser de mes de 9 caracters")
+        End If
+
+        If username.Text = 0 Then
+            MsgBox("S'ha d'escriure un nom d'usuari")
+            Return False
+        End If
+
+        If UsernameExists(username.Text) Then
+            MsgBox("Aquest nom d'usuari ja existeix")
+            Return False
+        End If
+
+        If postalcode.Text.Length > 0 Then
+            Try
+                Integer.Parse(postalcode.Text)
+            Catch ex As Exception
+                MsgBox("El codi postal ha de ser un número valid")
+                Return False
+            End Try
+        End If
+
+        Return True
+    End Function
+
+    Private Function UsernameExists(username As String)
+        For idx As Integer = 0 To dts.Tables("Members").Rows.Count - 1
+            If Not idx = bmb.Position Then
+                If username = dts.Tables("Members").Rows(idx)("USERNAME").ToString() Then
+                    Return True
+                End If
+            End If
+        Next
+        Return False
+    End Function
+
+    Private Sub EmptyMember(bool As Boolean)
+        firstname.ReadOnly = bool
+        lastname.ReadOnly = bool
+        address1.ReadOnly = bool
+        address2.ReadOnly = bool
+        state.ReadOnly = bool
+        postalcode.ReadOnly = bool
+        country.ReadOnly = bool
+        username.ReadOnly = bool
+        password.ReadOnly = bool
+        buscar_box.ReadOnly = bool
+
+        eliminar.Enabled = Not bool
+        editar.Enabled = Not bool
+        buscar.Enabled = Not bool
+    End Sub
+
+    Private Sub GenerateNewID()
+        bmb.AddNew()
+        Try
+            memberid.Text = dts.Tables("Members").Rows(dts.Tables("Members").Rows.Count - 1)("MEMBERID") + 1
+        Catch ex As Exception
+            memberid.Text = "1"
+        End Try
+
+    End Sub
+
+    Private Sub SaveChanges()
+        Try
+            adapter.Update(dts.Tables("Members").GetChanges())
+            dts.Tables("Members").AcceptChanges()
+            MsgBox("S'ha realitzat la persistencia amb exit")
+        Catch ex As Exception
+            MsgBox("S'ha realitzat la persistencia amb exit")
+        End Try
+    End Sub
+
+    Private Sub primer_Click(sender As Object, e As EventArgs) Handles primer.Click
+        bmb.Position = 0
+        updateControllers()
+    End Sub
+
+    Private Sub ultim_Click(sender As Object, e As EventArgs) Handles ultim.Click
+        bmb.Position = bmb.Count - 1
+        updateControllers()
+    End Sub
+
+    Private Sub anterior_Click(sender As Object, e As EventArgs) Handles anterior.Click
+        bmb.Position = bmb.Position - 1
+        updateControllers()
+    End Sub
+
+    Private Sub seguent_Click(sender As Object, e As EventArgs) Handles seguent.Click
+        bmb.Position = bmb.Position + 1
+        updateControllers()
+    End Sub
+
+    Private Sub memberid_KeyDown(sender As Object, e As KeyEventArgs) Handles memberid.KeyDown
+
+        If e.Alt Then
+            If e.KeyCode = Keys.Up Then
+                bmb.Position = 0
+            ElseIf e.KeyCode = Keys.Down Then
+                If bmb.Count > 0 Then
+                    bmb.Position = bmb.Count - 1
+                End If
+            ElseIf e.KeyCode = Keys.Left Then
+                If bmb.Position > 0 Then
+                    bmb.Position = bmb.Position - 1
+                End If
+            ElseIf e.KeyCode = Keys.Right Then
+                If bmb.Position < bmb.Count - 1 Then
+                    bmb.Position = bmb.Position + 1
+                End If
+            End If
+            updateControllers()
+        End If
+
+    End Sub
+
+    Private Sub updateControllers()
+        Leftcontrols(False)
+        RightControls(False)
+
+        If bmb.Count = 0 Then
+            Return
+        End If
+
+        If Not bmb.Position = bmb.Count - 1 Then
+            RightControls(True)
+        End If
+
+        If bmb.Position > 0 Then
+            Leftcontrols(True)
+        End If
+
+    End Sub
+
+    Private Sub genusername_Click(sender As Object, e As EventArgs) Handles genusername.Click
+        If firstname.Text.Length = 0 Or lastname.Text.Length = 0 Then
+            MsgBox("Per poder crear un username es necesari emplenar les dades de firstname i lastname")
+            Return
+        End If
+
+        If firstname.Text.Length < 3 Or lastname.Text.Length < 3 Then
+            MsgBox("Per poder crear un username es necesari que firstname i lastname tinguin mes de 3 caracters")
+            Return
+        End If
+
+        username.Text = firstname.Text.Substring(0, 3) + lastname.Text.Substring(lastname.Text.Length - 3, 3)
+    End Sub
+
+    Private Sub eliminar_Click(sender As Object, e As EventArgs) Handles eliminar.Click
+        If memberid.Text.Length > 0 Then
+            If MessageBox.Show("Estas segur que vols eliminar el membre?", "Atenció", MessageBoxButtons.YesNo, MessageBoxIcon.Question) = DialogResult.Yes Then
+                bmb.RemoveAt(bmb.Position)
+                SaveChanges()
+                updateControllers()
+
+                If bmb.Count = 0 Then
+                    EmptyMember(True)
+                End If
+            End If
+        End If
+    End Sub
+
+    Private Sub buscar_Click(sender As Object, e As EventArgs) Handles buscar.Click
+        For idx As Integer = 0 To dts.Tables("Members").Rows.Count - 1
+            If buscar_box.Text = dts.Tables("Members").Rows(idx)("MEMBERID").ToString() Then
+                bmb.Position = idx
+                updateControllers()
+                Return
+            End If
+        Next
+
+    End Sub
+
+    Private Sub editar_Click(sender As Object, e As EventArgs) Handles editar.Click
+        If Not VerifyMember() Then
+            Return
+        End If
+
+        bmb.EndCurrentEdit()
+        SaveChanges()
+        updateControllers()
+    End Sub
+End Class
